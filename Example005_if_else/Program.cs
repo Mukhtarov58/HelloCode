d6668550d8753e15170cca1ceee18688ba0f19c2
Timur <timur.pnz@mail.ru>
2023-09-12 23:03:12 +0300

    new folder Example005_if_else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "тимур")
+{
+    Console.WriteLine("Ура, это же Тимур!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
