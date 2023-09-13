3a5dd579920d6a83c8d9304101f12f947235fada
Timur <timur.pnz@mail.ru>
2023-09-13 22:08:09 +0300

    add new magik

---
+++ Example008_introMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 22;
+int b1 = 232;
+int c1 = 2244;
+int a2 = 252;
+int b2 = 26322;
+int c2 = 244;
+int a3 = 22;
+int b3 = 262;
+int c3 = 44;
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1, 234, 345, 55 , 4, 23, 1, 55};
+
+int n = array.Length;
+int find = 55;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
