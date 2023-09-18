770595fb34255a1f9bd996d1c86e7f3c306a14ac
Timur <timur.pnz@mail.ru>
2023-09-18 20:07:26 +0300

    Added new folders

---
+++ Seminar001_NewCode/Program.cs
@@ -0,0 +1,14 @@
+﻿// Console.WriteLine("Input a number: ");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int res = num * num;
+// Console.WriteLine($"Quater of {num} is {res}");
+
+
+Console.WriteLine("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int current = n * (-1);
+for( int i = current; i<= n; i++){
+    Console.Write($" {i} ");
+}
