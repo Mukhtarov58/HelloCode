c11cf426404c6a99d72b01e21f66c0bb4f851dcd
Timur <timur.pnz@mail.ru>
2023-09-18 22:09:04 +0300

    Added new folders Seminar002

---
+++ Seminar002_Metods/Program.cs
@@ -0,0 +1,34 @@
+﻿// void HelloFunc (string name) {
+//     Console.WriteLine($"Hello, {name}! How are you?");
+// }
+
+// Console.WriteLine("Hi, what is your name?");
+// string fio = Console.ReadLine();
+// HelloFunc(fio);
+
+// int Quater(int num = 0){
+//     int res = num * num;
+//     return res;
+// }
+
+//  Console.WriteLine("Enter the number: ");
+//  int number = Convert.ToInt32(Console.ReadLine());
+//  int quat = Quater(number);
+//  quat += 2;
+//  Console.WriteLine($"Your number with quater is {quat}");
+
+
+// void MaxDigit(int num){
+//     int digit1 = num /10;
+//     int digit2 = num %10;
+//     if (digit1>digit2)
+//         Console.WriteLine(digit1);
+//     else
+//         Console.WriteLine(digit2);
+// }
+//     int number = new Random().Next(10,100);
+//     ConsoleWriteLine($"Your number is{number}");
+//     MaxDigit(number);
+
+        
+  
