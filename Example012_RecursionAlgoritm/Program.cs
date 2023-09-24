8f64652f83a8881990bd05fa3ead54dbd4c73764
Timur <timur.pnz@mail.ru>
2023-09-24 22:18:32 +0300

    refactoring code

--- Example012_RecursionAlgoritm/Program.cs
+++ Example012_RecursionAlgoritm/Program.cs
@@ -57,10 +57,10 @@
 // }
 
 
-double Fibonacci (int n)
+double Fibonacci(int n)
 {
-    if (n == 1 || n ==2) return 1;
-    else return Fibonacci(n-1) + Fibonacci(n-2);
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 
 for (int i = 1; i < 50; i++)
--- Seminare003_magik/Program.cs
+++ Seminare003_magik/Program.cs
@@ -74,16 +74,18 @@
 //-5 -> 1, 4, 9, 16, 25.
 
 
-void Square(int num){
-    for(int i = 1; i<=num; i++){
-        Console.Write($"{i*i} ");
+void Square(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write($"{i * i} ");
     }
 }
 
 Console.WriteLine("Enter a number: ");
 int numb = Convert.ToInt32(Console.ReadLine());
-if(numb < 0)
+if (numb < 0)
     numb *= (-1);
-else if(numb==0)
+else if (numb == 0)
     Console.WriteLine("Wrong number");
 Square(numb);
